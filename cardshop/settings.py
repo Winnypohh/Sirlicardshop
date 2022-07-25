d030bcb2a7cba13f1eda2ac9cb66c73825fd3d9d
Winnypohh <maik523@hotmail.com>
2022-07-25 18:06:11 +0000

    error in Heroku

--- cardshop/settings.py
+++ cardshop/settings.py
@@ -205,7 +205,9 @@ STANDARD_DELIVERY_PERCENTAGE = 10
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CSRF_TRUSTED_ORIGINS = [
-    'https://8000-winnypohh-sirlicardshop-wm3vv5dr510.ws-eu54.gitpod.io',
+    'https://8000-winnypohh-sirlicardshop-wm3vv5dr510.ws-eu54.gitpod.io']
+
+CSRF_TRUSTED_ORIGINS = [
     'https://sirlishop.herokuapp.com/']
 
 CURRENCY_CHOICES = [('EUR', 'EUR €')]
